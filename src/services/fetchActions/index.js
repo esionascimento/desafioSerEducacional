91fd154a8deb502e3f6437af2f7a5231b25031ae
esionascimento <esio.392@protonmail.com>
2021-11-19 04:11:20 -0400

    criado arquivo usando axios para obter dados da api

---
+++ src/services/fetchActions/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+/* import baseURL from '../../services/services'; */
+import { Login } from '../../store/auth/Auth.action';
+
+export const newCadastro = (user) => {
+  axios.post('https://desafio-ser-educacional-back.herokuapp.com/user', user)
+  .then((res) => {
+    console.log('res :', res);
+    console.log(res.data);
+  })
+  .catch(console.log);
+};
+
+export const authLogin = (user) => {
+  return (dispatch) => {
+    axios.post('https://desafio-ser-educacional-back.herokuapp.com/login', user)
+      .then((res) => {
+        console.log('res :', res);
+        localStorage.setItem('token', res.data.token);
+        dispatch(Login());
+
+        window.location.pathname = '/dashboard';
+      })
+      .catch(console.log);
+  };
+};
