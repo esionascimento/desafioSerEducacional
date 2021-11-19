0b05ab106d4ac314b7e0ad5f14800d180220a15a
esionascimento <esio.392@protonmail.com>
2021-11-19 06:04:05 -0400

    função para conferir expiração token

--- src/util/verifyLogin.js
+++ src/util/verifyLogin.js
@@ -1,6 +1,25 @@
+import { useDispatch } from 'react-redux';
+import { useJwt } from "react-jwt";
+import { Login } from '../store/auth/Auth.action';
+
 export function verifyLogin({ email, password }) {
   if (email === 'admin' && password === 'admin') {
     return { token: '1234' };
   }
   return { error: 'Email ou senha invalido' };
+}
+
+export const verifyJWT = () => {
+  const dispatch = useDispatch();
+  const token = localStorage.getItem('token');
+  let isAuthenticated = false;
+  
+  if (token) {
+    const { isExpired } = useJwt(token);
+    if (!isExpired) {
+      dispatch(Login());
+      isAuthenticated = true;
+    }
+  }
+  return isAuthenticated;
 }
