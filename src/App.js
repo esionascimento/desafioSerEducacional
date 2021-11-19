9297a0b6ef590fa9e46783188808b74a32f164a8
esionascimento <esio.392@protonmail.com>
2021-11-19 04:08:53 -0400

    atualizado as rotas + rotas privatas / acesso somente com cadastro

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import store from './store/store';
+import store from './store';
 import { Provider } from 'react-redux';
 
-import Home from './pages/Home'
+import RoutesPrivate from './services/Private/Private';
+import { LoginAuth } from './services/Private/LoginAuth';
+
+import Home from './pages/Home/index.js'
+import Cadastro from './components/cadastro'
+import Dashboard from './pages/Dashboard/index.js';
+import NotFound from './pages/NotFound.js';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Provider store={store}>
           <Switch>
-            <Route exact path="/" component={Home} />
+            <LoginAuth exact path="/" component={Home} />
+            <RoutesPrivate exact path="/dashboard" component={() => <Dashboard />} />
+            <Route exact path="/cadastro" component={() => <Cadastro />} />
+            <Route exact path="/not-found" component={() => <NotFound />} />
           </Switch>
       </Provider>
     </BrowserRouter>
