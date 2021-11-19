ae7e28c389b42acaba2ee15d56b63cf531c2792d
esionascimento <esio.392@protonmail.com>
2021-11-19 04:10:48 -0400

    criado pagina para dashboard e home + page para not found

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
+import Login from '../../components/login';
+
+import './Home.css'
+
 function Home() {
   return (
     <>
+    
       <div className="box">
         <div className="container">
           <div className="left">
             <h1>Desafio Ser Educacional</h1>
           </div>
           <div className="right">
+            <Login />
           </div>
         </div>
       </div>
       <footer className="rodape">
-        Footer
+        Web site desenvolvido por Esio Rodrigues
       </footer>
     </>
   );
