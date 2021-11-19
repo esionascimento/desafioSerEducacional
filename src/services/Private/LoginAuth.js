996ea78785293a0d7acbadfb569a077d5f09eb4b
esionascimento <esio.392@protonmail.com>
2021-11-19 04:13:09 -0400

    incrementado dois arquivos de private, caso usuario já tenha logado pouco tempo antes, ou a caso tenha expirado token

---
+++ src/services/Private/LoginAuth.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+export const LoginAuth = ({ component: Component, ...rest }) => {
+  const { isAuthenticated } = useSelector(state => state.auth);
+  return (
+    <Route
+      {...rest}
+      render={(props) => isAuthenticated
+        ? <Redirect to={{ pathname: "/dashboard", state: { from: props.location } } }/>
+        : <Component {...rest} />
+      }
+    />
+  )
+}
+
+LoginAuth.propTypes = {
+  component: PropTypes.func.isRequired,
+  location: PropTypes.func,
+}
---
+++ src/services/Private/Private.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const RoutesPrivate = ({ component: Component, ...rest }) => {
+  const { isAuthenticated } = useSelector(state => state.auth);
+  
+  return (
+    <Route
+      {...rest}
+      render={(props) => isAuthenticated
+        ? <Component {...rest} />
+        : <Redirect to={{ pathname: "/not-found", state: { from: props.location } } }/>
+      }
+    />
+  )
+}
+
+RoutesPrivate.propTypes = {
+  component: PropTypes.func.isRequired,
+  location: PropTypes.func,
+}
+
+export default RoutesPrivate;
