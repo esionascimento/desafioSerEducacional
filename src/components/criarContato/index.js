d2360b6f7f58155eb2a5c2ba4ca0e1c426cf1967
esionascimento <esio.392@protonmail.com>
2021-11-23 03:59:32 -0400

    function para preenchimento do forms para novo contato

---
+++ src/components/criarContato/index.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { SalveFormContato } from '../../store/dashboard/Dashboard.actions';
+
+import './criarContato.css';
+
+export function FormContato() {
+  const dispatch = useDispatch();
+  const [form, setForm] = useState({
+    nome: '',
+    sobrenome: '',
+    telefone: '',
+    dataNascimento: '',
+    endereco: '',
+    email: '',
+  });
+
+  useEffect(() => {
+    dispatch(SalveFormContato(form));
+  },[form]);
+
+  function onChange(event) {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
+
+  return (
+    <>
+      <div className="formContato">
+        <div>
+          <label>Nome: </label>
+          <input type="text" name="nome" onChange={onChange}></input>
+        </div>
+        <div>
+          <label>Sobrenome: </label>
+          <input type="text" name="sobrenome" onChange={onChange}></input>
+        </div>
+        <div>
+          <label>Telefone: </label>
+          <input type="number" name="telefone" onChange={onChange}></input>
+        </div>
+        <div>
+          <label>Data de nascimento: </label>
+          <input type="date" name="dataNascimento" onChange={onChange}></input>
+        </div>
+        <div>
+          <label>Endereço: </label>
+          <input type="email" name="endereco" onChange={onChange}></input>
+        </div>
+        <div>
+          <label>Email: </label>
+          <input type="email" name="email" onChange={onChange}></input>
+        </div>
+      </div>
+      <p>O modal será fechado após dois segundos</p>
+    </>
+  );
+}
