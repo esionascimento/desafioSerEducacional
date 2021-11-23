46d993264225b3e01c1e32185a6f1009403c97bb
esionascimento <esio.392@protonmail.com>
2021-11-23 04:00:22 -0400

    increment modal da lib antd

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,20 +1,49 @@
 import React from 'react';
+import { Modal } from 'antd';
+import 'antd/dist/antd.css';
+
 import NomesContatos from '../../components/nomesContatos';
 import DetalhesContatos from '../../components/detalhesContatos';
+import { FormContato } from '../../components/criarContato';
 
 import './Dashboard.css';
 
 function Dashboard() {
-  
+  const [visible, setVisible] = React.useState(false);
+  const [confirmLoading, setConfirmLoading] = React.useState(false);
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleOk = () => {
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
   return (
     <>
+      <Modal
+        title="Novo Contato"
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        {FormContato()}
+      </Modal>
+
       <header className="app-header">
         <ul className="div">
-          <li>Novo contato</li>
+          <li onClick={showModal}>Novo contato</li>
         </ul>
         <ul className="div">
-          <li>Saiba Mais</li>
-          <li>Novidades</li>
           <li>Sair</li>
         </ul>
       </header>
